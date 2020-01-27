876cc8d3877f4b29fd58bbd9f70fb54c0d38502e
PanteraSLS <panterasls123@gmail.com>
2020-01-27 12:22:50 +0200

    added currency

--- CurrencyManager/Controllers/CurrencyTypeTableViewController.swift
+++ CurrencyManager/Controllers/CurrencyTypeTableViewController.swift
@@ -10,13 +10,45 @@ import UIKit
 
 class CurrencyTypeTableViewController: UITableViewController {
     
-    private var currencyArray = [(symbol: String, name: String)]()
+    private var currencyArray: [(symbol: String, name: String)] = [
+        ("$", "USD"), ("€", "EURO"), ("₽", "RUB"), ("Lek", "ALL"), ("؋", "AFN"),
+        ("$", "ARS"), ("ƒ", "AWG"), ("$", "AUD"), ("₼", "AZN"), ("$", "BSD"),
+        ("$", "BBD"), ("Br", "BYN"), ("BZ$", "BZD"), ("$", "BMD"), ("$b", "BOB"),
+        ("KM", "BAM"), ("", "BWP"), ("лв", "BGN"), ("R$", "BRL"), ("$", "BND"),
+        ("៛", "KHR"), ("$", "CAD"), ("$", "KYD"), ("$", "CLP"), ("¥", "CNY"),
+        ("$", "COP"), ("₡", "CRC"), ("kn", "HRK"), ("₱", "CUP"), ("Kč", "CZK"),
+        ("kr", "DKK"), ("RD$", "DOP"), ("$", "XCD"), ("£", "EGP"), ("$", "SVC"),
+        ("£", "FKP"), ("$", "FJD"), ("¢", "GHS"), ("£", "GIP"), ("Q", "GTQ"), ("£", "GGP"),
+        ("$", "GYD"), ("L", "HNL"), ("$", "HKD"), ("Ft", "HUF"), ("kr", "ISK"), ("₹", "INR"),
+        ("Rp", "IDR"), ("﷼", "IRR"), ("£", "IMP"), ("₪", "ILS"), ("J$", "JMD"), ("¥", "JPY"),
+        ("£", "JEP"), ("лв", "KZT"), ("₩", "KPW"), ("₩", "KRW"), ("лв", "KGS"), ("₭", "LAK"),
+        ("£", "LBP"), ("$", "LRD"), ("ден", "MKD"), ("RM", "MYR"), ("₨", "MUR"), ("$", "MXN"),
+        ("₮", "MNT"), ("MT", "MZN"), ("$", "NAD"), ("₨", "NPR"), ("ƒ", "ANG"), ("$", "NZD"),
+        ("C$", "NIO"), ("₦", "NGN"), ("kr", "NOK"), ("﷼", "OMR"), ("₨", "PKR"), ("B/.", "PAB"),
+        ("Gs", "PYG"), ("S/.", "PEN"), ("₱", "PHP"), ("zł", "PLN"), ("﷼", "QAR"), ("lei", "RON"),
+        ("£", "SHP"), ("﷼", "SAR"), ("Дин.", "RSD"), ("₨", "SCR"), ("$", "SGD"), ("$", "SBD"),
+        ("S", "SOS"), ("R", "ZAR"), ("₨", "LKR"), ("kr", "SEK"), ("CHF", "CHF"), ("$", "SRD"),
+        ("£", "SYP"), ("NT$", "TWD"), ("฿", "THB"), ("TT$", "TTD"), ("₺", "TRY"), ("$", "TVD"),
+        ("₴", "UAH"), ("£", "GBP"), ("$U", "UYU"), ("лв", "UZS"), ("Bs", "VEF"),
+        ("₫", "VND"), ("﷼", "YER"), ("Z$", "ZWD")]
+    private var currencyDictionary = [String: [String]]()
+    private var currencySectionTitles = [String]()
     private var currentCurrency = ""
     private let index = UserDefaults.standard
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        currencyArray = [("$", "USD"), ("€", "EURO"), ("₽", "RUB")]
+        for (_, value) in currencyArray {
+            let currKey = String(value.prefix(1))
+            if var currValues = currencyDictionary[currKey] {
+                currValues.append(value)
+                currencyDictionary[currKey] = currValues
+            } else {
+                currencyDictionary[currKey] = [value]
+            }
+        }
+        currencySectionTitles = [String](currencyDictionary.keys)
+        currencySectionTitles = currencySectionTitles.sorted(by: { $0 < $1 })
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -33,17 +65,34 @@ class CurrencyTypeTableViewController: UITableViewController {
 extension CurrencyTypeTableViewController {
     
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
+        return currencySectionTitles.count
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return currencyArray.count
+        let currKey = currencySectionTitles[section]
+        if let currValues = currencyDictionary[currKey] {
+            return currValues.count
+        }
+        return 0
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return currencySectionTitles[section]
+    }
+    
+     override func sectionIndexTitles(for tableView: UITableView) -> [String]? {
+        return currencySectionTitles
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "currCell", for: indexPath) as! CurrencyTypeTableViewCell
-        cell.currSymbolLabel.text = currencyArray[indexPath.row].symbol
-        cell.currNameLabel.text = currencyArray[indexPath.row].name
+        
+        let currKey = currencySectionTitles[indexPath.section]
+        if let currValues = currencyDictionary[currKey] {
+            cell.currSymbolLabel.text = currencyArray[indexPath.row].symbol
+            cell.currNameLabel.text = currValues[indexPath.row]
+        }
+        
         let i = IndexPath(row: index.integer(forKey: "index"), section: 0)
         if flag {
             if indexPath == myIndexPath {
